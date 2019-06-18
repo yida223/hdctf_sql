6623ce9dde1b8bf198a51ba0131c767cb4d77ad6
yida223 <42206940+yida223@users.noreply.github.com>
2019-06-18 15:32:36 +0800

    Add files via upload

---
+++ js/forms.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+    var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+    var blue = document.querySelector('.js-switch-blue');
+    var switchery = new Switchery(blue, {
+        color: '#17c3e5'
+    });
+    var subdomain = document.querySelector('.js-switch-subdomain');
+    var switchery = new Switchery(subdomain, {
+        color: '#17c3e5',
+    });
+    var portscan = document.querySelector('.js-switch-portscan');
+    var switchery = new Switchery(portscan, {
+        color: '#17c3e5',
+    });
+    var extend = document.querySelector('.js-switch-extend');
+    var switchery = new Switchery(extend, {
+        color: '#17c3e5',
+    });
+    var fingerprint = document.querySelector('.js-switch-fingerprint');
+    var switchery = new Switchery(fingerprint, {
+        color: '#17c3e5',
+    });
+    var burp_system = document.querySelector('.js-switch-burp_system');
+    var switchery = new Switchery(burp_system, {
+        color: '#17c3e5',
+    });
+    var infoscan = document.querySelector('.js-switch-infoscan');
+    var switchery = new Switchery(infoscan, {
+        color: '#17c3e5',
+    });
+    var spider = document.querySelector('.js-switch-spider');
+    var switchery = new Switchery(spider, {
+        color: '#17c3e5',
+    });
+    var plugin = document.querySelector('.js-switch-plugin');
+    var switchery = new Switchery(plugin, {
+        color: '#17c3e5',
+    });
+    var secondary = document.querySelector('.js-switch-secondary');
+    var switchery = new Switchery(secondary, {
+        color: '#FFB244',
+        secondaryColor: '#ff8787'
+    });
+    var switch1 = document.querySelector('.js-switch-1');
+    var switchery = new Switchery(switch1, {
+        color: '#17c3e5',
+    });
+    var switch2 = document.querySelector('.js-switch-2');
+    var switchery = new Switchery(switch2, {
+        color: '#17c3e5',
+    });
+    var switch3 = document.querySelector('.js-switch-3');
+    var switchery = new Switchery(switch3, {
+        color: '#17c3e5',
+    });
+    var switch4 = document.querySelector('.js-switch-4');
+    var switchery = new Switchery(switch4, {
+        color: '#17c3e5',
+    });
+    var switch5 = document.querySelector('.js-switch-5');
+    var switchery = new Switchery(switch5, {
+        color: '#17c3e5',
+    });
+    var switch6 = document.querySelector('.js-switch-6');
+    var switchery = new Switchery(switch6, {
+        color: '#17c3e5',
+    });
+    var switch7 = document.querySelector('.js-switch-7');
+    var switchery = new Switchery(switch7, {
+        color: '#17c3e5',
+    });
+    var switch8 = document.querySelector('.js-switch-8');
+    var switchery = new Switchery(switch8, {
+        color: '#17c3e5',
+        disabled: true,
+        disabledOpacity: 0.5
+    });
+});
